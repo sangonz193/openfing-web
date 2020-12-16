03d070e75d3d08b04a0354e6bbcb4da583b9201f
Santiago González <37671738+sangonz193@users.noreply.github.com>
2020-12-16 00:49:21 -0300

    ci fixes

--- cli/commands/generate-files/_utils/generateFilesConfig.ts
+++ cli/commands/generate-files/_utils/generateFilesConfig.ts
@@ -4,8 +4,6 @@ import { projectPath } from "../../../_utils/projectPath";
 
 const { API_CLIENT_URI } = process.env;
 
-if (!API_CLIENT_URI) throw new Error(`API_CLIENT_URI is not defined`);
-
 export const generateFilesConfig = {
 	graphqlEndpoint: API_CLIENT_URI,
 	generatedFolderPath: path.resolve(projectPath, "src", "generated"),
