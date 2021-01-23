270535df8d328d683e6f16ac6d25b2705c12f04d
Santiago González <37671738+sangonz193@users.noreply.github.com>
2021-01-23 20:35:59 -0300

    courseSearchStore from mobx to apollo client

--- src/modules/CourseSearch/CourseSearch.store.ts
+++ src/modules/CourseSearch/CourseSearch.store.ts
@@ -1,49 +1,57 @@
+import { makeVar } from "@apollo/client";
 import FuzzySearch from "fuzzy-search";
-import { action, computed, observable } from "mobx";
 
+import { listenVar } from "../../_utils/listenVar";
 import { CourseFragment_CourseSearchFragment } from "./CourseSearch.graphql.generated";
 
 export class CourseSearchStore {
-	@observable source: CourseFragment_CourseSearchFragment[] = [];
-	@observable searchValue: string = "";
+	source = makeVar<CourseFragment_CourseSearchFragment[]>([]);
+	searchValue = makeVar<string>("");
+	searchResults = makeVar<CourseFragment_CourseSearchFragment[]>([]);
 
-	@action setSource(source: CourseFragment_CourseSearchFragment[]) {
-		this.source = source;
+	protected listeners: Array<() => void> = [];
+
+	constructor() {
+		this.listeners.push(listenVar(this.source, this.updateSearchResults));
+		this.listeners.push(listenVar(this.searchValue, this.updateSearchResults));
 	}
 
-	@action setSearchValue(searchValue: string) {
-		this.searchValue = searchValue;
+	dispose() {
+		this.listeners.forEach((listener) => listener());
 	}
 
-	@computed get searchResults() {
-		return this.searcher.search(
-			this.searchValue
-				.toLowerCase()
-				.replace(/á/g, "a")
-				.replace(/é/g, "e")
-				.replace(/í/g, "i")
-				.replace(/ó/g, "o")
-				.replace(/ú/g, "u")
-		);
-	}
-
-	@computed private get searcher() {
+	protected getSearcher() {
 		return new FuzzySearch(
-			this.source.map((c) => ({
-				id: c.id,
-				code: c.code,
-				name: c.name
+			this.source().map((course) => ({
+				id: course.id,
+				code: course.code,
+				name: course.name
 					.toLowerCase()
 					.replace(/á/g, "a")
 					.replace(/é/g, "e")
 					.replace(/í/g, "i")
 					.replace(/ó/g, "o")
 					.replace(/ú/g, "u"),
-			})) || [],
+			})),
 			["code", "name"],
 			{
 				sort: true,
 			}
 		);
 	}
+
+	private updateSearchResults = () => {
+		const searcher = this.getSearcher();
+		this.searchResults(
+			searcher.search(
+				this.searchValue()
+					.toLowerCase()
+					.replace(/á/g, "a")
+					.replace(/é/g, "e")
+					.replace(/í/g, "i")
+					.replace(/ó/g, "o")
+					.replace(/ú/g, "u")
+			)
+		);
+	};
 }
