dd1156a708b741b87e5e5308d2a36c4d4aec5abc
Santiago González <37671738+sangonz193@users.noreply.github.com>
2021-01-23 20:49:02 -0300

    add pin course class list button

--- src/components/Course/Course.graphql.generated.ts
+++ src/components/Course/Course.graphql.generated.ts
@@ -7,7 +7,7 @@ export type CourseClassListByCodeQueryVariables = Types.Exact<{
 }>;
 
 
-export type CourseClassListByCodeQuery = { __typename?: 'Query', courseClassListByCode: { __typename: 'CourseClassList', id: string, courseEdition: Types.Maybe<{ __typename?: 'CourseEdition', id: string, course: Types.Maybe<{ __typename?: 'Course', id: string, name: string, eva: Types.Maybe<string> }> }> } | { __typename: 'NotFoundError' } };
+export type CourseClassListByCodeQuery = { __typename?: 'Query', courseClassListByCode: { __typename: 'CourseClassList', id: string, courseEdition: Types.Maybe<{ __typename?: 'CourseEdition', id: string, course: Types.Maybe<{ __typename?: 'Course', id: string, name: string, eva: Types.Maybe<string> }> }>, classes: Types.Maybe<Array<{ __typename?: 'CourseClass', id: string, number: Types.Maybe<number> }>> } | { __typename: 'NotFoundError' } };
 
 
 export function useCourseClassListByCodeQuery(baseOptions?: Apollo.QueryHookOptions<CourseClassListByCodeQuery, CourseClassListByCodeQueryVariables>) {
