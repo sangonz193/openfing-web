409a721f1abe9be8009ead13f6706ea27b4dc8ef
Santiago González <37671738+sangonz193@users.noreply.github.com>
2021-01-23 22:54:10 -0300

    RootEventListeners from mobx to apollo client

--- src/modules/RootEventListeners/RootEventListeners.store.ts
+++ src/modules/RootEventListeners/RootEventListeners.store.ts
@@ -1,37 +1,60 @@
-import { computed, observable } from "mobx";
+import { makeVar } from "@apollo/client";
 
 export type EventsMap = Required<Omit<React.DOMAttributes<Element>, "children" | "dangerouslySetInnerHTML" | "css">>;
 
 export class RootEventListenersStore {
-	@observable listenersMap = new Map<keyof EventsMap, Array<EventsMap[keyof EventsMap]>>();
+	listenersMap = makeVar<{ [K in keyof EventsMap]?: Array<EventsMap[K]> }>({});
+	listeners = makeVar<Partial<EventsMap>>({});
 
-	@computed get listeners(): Partial<EventsMap> {
-		const entries = [...this.listenersMap.entries()];
+	constructor() {
+		this.listenersMap.onNextChange((newValue) => {
+			const entries =
+				((Object.entries(newValue) as unknown) as Array<
+					Exclude<
+						{
+							[K in keyof typeof newValue]: [K, Exclude<typeof newValue[K], undefined>];
+						}[keyof typeof newValue],
+						undefined
+					>
+				>) || [];
 
-		return entries.reduce<Partial<EventsMap>>((res, [key, listeners]) => {
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			res[key] = (event: any) => {
-				listeners.forEach((listener) => {
-					listener(event);
-				});
-			};
+			this.listeners(
+				entries.reduce<Partial<EventsMap>>((res, [key, listeners]) => {
+					// eslint-disable-next-line @typescript-eslint/no-explicit-any
+					res[key] = (event: any) => {
+						// eslint-disable-next-line @typescript-eslint/no-explicit-any
+						listeners.forEach((listener: any) => {
+							listener(event);
+						});
+					};
 
-			return res;
-		}, {});
+					return res;
+				}, {})
+			);
+		});
 	}
 
 	addListener<T extends keyof EventsMap>(event: T, listener: EventsMap[T]) {
-		const listeners = this.listenersMap.get(event);
+		const listenersMap = this.listenersMap();
+		const listeners = listenersMap[event];
 
-		this.listenersMap.set(event, [...(listeners || []), listener]);
+		this.listenersMap({
+			...this.listenersMap(),
+			[event]: [...((listeners || []) as []), listener],
+		});
 	}
 
 	removeListener<T extends keyof EventsMap>(event: T, listener: EventsMap[T]) {
-		const listeners = [...(this.listenersMap.get(event) || [])];
-		const listenerIndex = listeners.indexOf(listener);
+		const listenersMap = this.listenersMap();
+		const listeners = listenersMap[event];
+		const listenerIndex = listeners?.indexOf(listener) ?? -1;
 
-		if (listenerIndex >= 0) listeners.splice(listenerIndex, 1);
+		if (listenerIndex >= 0) listeners?.splice(listenerIndex, 1);
 
-		this.listenersMap.set(event, listeners);
+		if (listeners)
+			this.listenersMap({
+				...this.listenersMap(),
+				[event]: listeners,
+			});
 	}
 }
