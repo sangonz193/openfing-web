7cbaafeff7bdda0a88d5497604a05db4e6453321
Santiago González <37671738+sangonz193@users.noreply.github.com>
2023-08-27 17:31:31 -0300

    add prettier hook to codegen

--- codegen.ts
+++ codegen.ts
@@ -8,6 +8,7 @@ const config: CodegenConfig = {
 			preset: "client",
 		},
 	},
+	hooks: { afterOneFileWrite: ["prettier --write"] },
 }
 
 export default config
--- config/commands/graphql-schema.js
+++ config/commands/graphql-schema.js
@@ -24,6 +24,7 @@ module.exports = {
 					plugins: ["schema-ast"],
 				},
 			},
+			hooks: { afterOneFileWrite: ["prettier --write"] },
 		})
 	},
 }
