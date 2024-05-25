ec9b3cbde5b5188f841cb1ab8d09213bc8e3dd44
Santiago González <37671738+sangonz193@users.noreply.github.com>
2024-05-25 04:15:02 -0300

    remove icon from metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { Analytics } from "@vercel/analytics/react"
 import { GeistSans } from "geist/font/sans"
 
 import { serverEnv } from "@/modules/env/server"
-import icon from "@/public/icon.svg"
 
 import { Providers } from "./providers"
 
@@ -16,7 +15,6 @@ const defaultUrl = serverEnv.VERCEL_URL
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "OpenFING",
-  icons: [{ rel: "icon", url: icon.src }],
 }
 
 export default function RootLayout({
